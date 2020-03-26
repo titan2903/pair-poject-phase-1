c8d1547eb8f7eee194800edc0cc396f9e0009184
Reza Ilham <reza@rebelworks.co>
2020-03-27 01:28:41 +0700

    views updated

--- pair-project-hacktiv-phase-1/app.js
+++ pair-project-hacktiv-phase-1/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 5000
+const port = 3000
 
 const routes = require('./routes/router')
 const session = require('express-session')
@@ -19,6 +19,10 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.static(__dirname + '/public'));
 
 app.use('/', routes);
+
+app.get('/term', (req, res) => {
+  res.redirect('term')
+})
 
 app.get('*', (req, res) => {
   res.send('404 Page Not Found!')
